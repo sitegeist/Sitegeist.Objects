cfa23131bdaf57dc4906e9f72f39a3615a680ab0
Wilhelm Behncke <2522299+grebaldi@users.noreply.github.com>
2018-05-01 15:27:59 +0200

    FEATURE: Create ObjectQuery and -Helper

---
+++ Classes/GraphQl/Query/ObjectHelper.php
@@ -0,0 +1,211 @@
+<?php
+namespace Sitegeist\Objects\GraphQl\Query;
+
+/*
+ * Copyright notice
+ *
+ * (c) 2018 Wilhelm Behncke <behncke@sitegeist.de>
+ * All rights reserved
+ *
+ * This file is part of the Sitegeist/Objects project under GPL-3.0.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.md file that was distributed with this source code.
+ */
+
+use Neos\Flow\Annotations as Flow;
+use Neos\ContentRepository\Domain\Model\NodeInterface;
+use Neos\ContentRepository\Domain\Model\NodeType;
+use Sitegeist\Objects\Service\NodeService;
+
+class ObjectHelper
+{
+    /**
+     * @var NodeType
+     */
+    protected $nodeType;
+
+    /**
+     * @var NodeInterface
+     */
+    protected $node;
+
+    /**
+     * @Flow\Inject
+     * @var NodeService
+     */
+    protected $nodeService;
+
+    /**
+     * Protected Constructor: Use factory methods below!
+     *
+     * @param NodeType $nodeType
+     * @param NodeInterface|null $node
+     * @throws \InvalidArgumentException
+     */
+    protected function __construct(NodeType $nodeType, NodeInterface $node = null)
+    {
+        //
+        // Invariant: Type of $node must be equal to type $nodeType
+        //
+        if ($node !== null && $node->getNodeType() !== $nodeType) {
+            throw new \InvalidArgumentException(
+                sprintf('Node must be of type "%s"', $nodeType->getName()),
+                1524931043
+            );
+        }
+
+        $this->nodeType = $nodeType;
+        $this->node = $node;
+
+        //
+        // Invariant: nodeType must be of type "Sitegeist.Objects:Object"
+        //
+        if (!$nodeType->isOfType('Sitegeist.Objects:Object')) {
+            throw new \InvalidArgumentException(
+                sprintf('NodeType "%s" must inherit from "Sitegeist.Objects:Object"', $nodeType->getName()),
+                1524931043
+            );
+        }
+    }
+
+    /**
+     * Factory method to create ObjectHelper from node type
+     *
+     * @param NodeType $nodeType
+     * @return ObjectHelper
+     */
+    public static function createFromNodeType(NodeType $nodeType)
+    {
+        return new static($nodeType);
+    }
+
+    /**
+     * Factory method to create ObjectHelper from node type
+     *
+     * @param NodeType $nodeType
+     * @return ObjectHelper
+     */
+    public static function createFromNode(NodeInterface $node)
+    {
+        return new static($node->getNodeType(), $node);
+    }
+
+    /**
+     * Get the node type
+     *
+     * @return NodeType
+     */
+    public function getNodeType() : NodeType
+    {
+        return $this->nodeType;
+    }
+
+    /**
+     * Check if node is set
+     *
+     * @return boolean
+     */
+    public function hasNode()
+    {
+        return $this->node !== null;
+    }
+
+    /**
+     * Get the icon
+     *
+     * @return string
+     */
+    public function getIcon()
+    {
+        return $this->nodeType->getConfiguration('ui.icon');
+    }
+
+    /**
+     * Get the label
+     *
+     * @return string
+     */
+    public function getLabel()
+    {
+        if ($this->hasNode()) {
+            return $this->node->getLabel();
+        }
+
+        return $this->nodeType->getLabel();
+    }
+
+    /**
+     * Get if the object node has unpublished changes
+     *
+     * @return boolean
+     */
+    public function getHasUnpublishedChanges()
+    {
+        if ($this->hasNode()) {
+            return $this->nodeService->checkIfNodeHasUnpublishedChanges($this->getNode());
+        }
+
+        return false;
+    }
+
+    /**
+     * @TODO: method comment
+     *
+     * @param array  $arguments
+     * @return mixed
+     */
+    public function getProperty(array $arguments)
+    {
+        if ($this->hasNode()) {
+            return $this->node->getProperty($arguments['name']);
+        }
+
+        $defaultValues = $this->nodeType->getDefaultValuesForProperties();
+
+        if (array_key_exists($arguments['name'], $defaultValues)) {
+            return $defaultValues[$arguments['name']];
+        }
+    }
+
+    /**
+     * @TODO: method comment
+     *
+     * @return array
+     */
+    public function getProperties()
+    {
+        if ($this->hasNode()) {
+            return $this->node->getProperties();
+        }
+
+        return $this->nodeType->getDefaultValuesForProperties();
+    }
+
+    /**
+     * Route all method calls to the node, if set
+     *
+     * @param string $name
+     * @param array $arguments
+     * @return mixed
+     */
+    public function __call($name, $arguments)
+    {
+        if (!is_array($arguments)) {
+            $arguments = [];
+        }
+
+        if (method_exists(NodeInterface::class, $name)) {
+            if ($this->hasNode()) {
+                return $this->node->{$name}(...$arguments);
+            }
+
+            return null;
+        }
+
+        throw new \BadMethodCallException(
+            sprintf('Method "%s" is not implemented in "%s"', $name, NodeInterface::class),
+            1525176050
+        );
+    }
+}
---
+++ Classes/GraphQl/Query/ObjectQuery.php
@@ -0,0 +1,72 @@
+<?php
+namespace Sitegeist\Objects\GraphQl\Query;
+
+/*
+ * Copyright notice
+ *
+ * (c) 2018 Wilhelm Behncke <behncke@sitegeist.de>
+ * All rights reserved
+ *
+ * This file is part of the Sitegeist/Objects project under GPL-3.0.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.md file that was distributed with this source code.
+ */
+
+use Neos\Flow\Annotations as Flow;
+use GraphQL\Type\Definition\ObjectType;
+use GraphQL\Type\Definition\Type;
+use GraphQL\Type\Definition\ResolveInfo;
+use Wwwision\GraphQL\TypeResolver;
+use Sitegeist\Objects\GraphQl\Scalar\JsonScalar;
+
+class ObjectQuery extends ObjectType
+{
+    /**
+     * @param TypeResolver $typeResolver
+     */
+    public function __construct(TypeResolver $typeResolver)
+    {
+        return parent::__construct([
+            'name' => 'Object',
+            'description' => 'An object',
+            'fields' => [
+                'identifier' => [
+                    'type' => Type::id(),
+                    'description' => 'The id of the object node or null if empty'
+                ],
+                'nodeType' => [
+                    'type' => $typeResolver->get(NodeTypeQuery::class),
+                    'description' => 'The node type of the object node'
+                ],
+                'icon' => [
+                    'type' => Type::string(),
+                    'description' => 'The icon of the object node'
+                ],
+                'label' => [
+                    'type' => Type::string(),
+                    'description' => 'The label of the object node or null if empty'
+                ],
+                'isHidden' => [
+                    'type' => Type::boolean(),
+                    'description' => 'Is the object hidden?'
+                ],
+                'isRemoved' => [
+                    'type' => Type::boolean(),
+                    'description' => 'Has the object been removed?'
+                ],
+                'hasUnpublishedChanges' => [
+                    'type' => Type::boolean(),
+                    'description' => 'Does the object have unpublished changes?'
+                ],
+                'properties' => [
+                    'type' => JsonScalar::type(),
+                    'description' => 'All properties of this object'
+                ]
+            ],
+            'resolveField'  => function(ObjectHelper $object, $arguments, $context, ResolveInfo $info) {
+                return $object->{'get' . ucfirst($info->fieldName)}($arguments);
+            }
+        ]);
+    }
+}
