7e600ae65c2158fe994807d0eb3e2e3ff594d84a
Wilhelm Behncke <2522299+grebaldi@users.noreply.github.com>
2018-05-08 15:51:10 +0200

    FEATURE: Create buttonList primitive

---
+++ Resources/Private/JavaScript/ui/primitives/buttonList.js
@@ -0,0 +1,24 @@
+/*
+ * Copyright notice
+ *
+ * (c) 2018 Wilhelm Behncke <behncke@sitegeist.de>
+ * All rights reserved
+ *
+ * This file is part of the Sitegeist/Objects project under GPL-3.0.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.md file that was distributed with this source code.
+ */
+import styled from 'styled-components';
+
+import Button from './button';
+
+const ButtonList = styled.div`
+	margin-bottom: 2em!important;
+
+	${Button} {
+		margin-right: 10px;
+	}
+`;
+
+export default ButtonList;
