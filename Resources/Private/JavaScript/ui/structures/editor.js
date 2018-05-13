a9fb311fd1f781e1d741c1fde2489122cc00a25e
Wilhelm Behncke <wilhelm.behncke@protonmail.com>
2018-05-13 14:22:59 +0200

    FEATURE: Create component that will be suitable as a skeleton for most editors

---
+++ Resources/Private/JavaScript/ui/structures/editor.js
@@ -0,0 +1,46 @@
+/*
+ * Copyright notice
+ *
+ * (c) 2018 Wilhelm Behncke <behncke@sitegeist.de>
+ * All rights reserved
+ *
+ * This file is part of the Sitegeist/Objects project under GPL-3.0.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.md file that was distributed with this source code.
+ */
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const Container = styled.div`
+	margin-bottom: 1em!important;
+	flex: 1;
+`;
+
+const Label = styled.label`
+`;
+
+const Body = styled.div`
+	position: relative;
+`;
+
+const Editor = ({id, label, children}) => (
+	<Container>
+		<Label htmlFor={id}>
+			{label}:
+		</Label>
+
+		<Body>
+			{children}
+		</Body>
+	</Container>
+);
+
+Editor.propTypes = {
+	id: PropTypes.string.isRequired,
+	label: PropTypes.string.isRequired,
+	children: PropTypes.node.isRequired
+};
+
+export default Editor;
