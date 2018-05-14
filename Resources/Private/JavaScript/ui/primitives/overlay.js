5726cf0fd373c34761b19818632a962033488c48
Wilhelm Behncke <wilhelm.behncke@protonmail.com>
2018-05-14 13:26:31 +0200

    FEATURE: Create overlay primitive

---
+++ Resources/Private/JavaScript/ui/primitives/overlay.js
@@ -0,0 +1,26 @@
+/*
+ * Copyright notice
+ *
+ * (c) 2018 Wilhelm Behncke <behncke@sitegeist.de>
+ * All rights reserved
+ *
+ * This file is part of the Sitegeist/Objects project under GPL-3.0.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.md file that was distributed with this source code.
+ */
+import styled from 'styled-components';
+
+const Overlay = styled.div`
+	position: fixed;
+	top: 0;
+	left: 0;
+	bottom: 0;
+	right: 0;
+	background-color: rgba(0, 0, 0, .7);
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`;
+
+export default Overlay;
