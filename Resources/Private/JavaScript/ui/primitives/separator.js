252fe976a932089bc0665102476bfb83cb530575
Wilhelm Behncke <wilhelm.behncke@protonmail.com>
2018-05-14 13:26:13 +0200

    FEATURE: Create separator primitive

---
+++ Resources/Private/JavaScript/ui/primitives/separator.js
@@ -0,0 +1,20 @@
+/*
+ * Copyright notice
+ *
+ * (c) 2018 Wilhelm Behncke <behncke@sitegeist.de>
+ * All rights reserved
+ *
+ * This file is part of the Sitegeist/Objects project under GPL-3.0.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.md file that was distributed with this source code.
+ */
+import styled from 'styled-components';
+
+const Separator = styled.hr`
+	border: none;
+	border-bottom: 1px solid #3f3f3f;
+	margin: 10px 0;
+`;
+
+export default Separator;
