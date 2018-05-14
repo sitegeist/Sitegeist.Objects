3abd335dd5a117b7e080319f1c48b9201b17ef43
Wilhelm Behncke <wilhelm.behncke@protonmail.com>
2018-05-14 13:27:51 +0200

    FEATURE: Create confirm util

---
+++ Resources/Private/JavaScript/core/util/confirm.js
@@ -0,0 +1,92 @@
+/*
+ * Copyright notice
+ *
+ * (c) 2018 Wilhelm Behncke <behncke@sitegeist.de>
+ * All rights reserved
+ *
+ * This file is part of the Sitegeist/Objects project under GPL-3.0.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.md file that was distributed with this source code.
+ */
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+import Modal from '../../ui/primitives/modal';
+import Button from '../../ui/primitives/button';
+import ButtonList from '../../ui/primitives/buttonList';
+import Separator from '../../ui/primitives/separator';
+
+import Toggle from './toggle';
+import Condition from './condition';
+
+export default class Confirm extends Component {
+	static propTypes = {
+		question: PropTypes.string.isRequired,
+		onConfirm: PropTypes.func,
+		onCancel: PropTypes.func,
+		children: PropTypes.func
+	}
+
+	static defaultProps = {
+		onConfirm: () => {},
+		onCancel: () => {},
+		children: () => null
+	}
+
+	handleConfirmation = () => {
+		const {onConfirm} = this.props;
+
+		onConfirm();
+	};
+
+	handleCancellation = () => {
+		const {onCancel} = this.props;
+
+		onCancel();
+	};
+
+	render() {
+		return (
+			<Toggle initial={false}>
+				{open => (
+					<React.Fragment>
+						<Condition condition={open.is}>
+							<Modal>
+								{this.props.question}
+								<Separator/>
+								<ButtonList>
+									<Button
+										className="neos-button-danger"
+										onClick={() => {
+											open.setFalse();
+											this.handleConfirmation();
+										}}
+									>
+										{/* @TODO: I18n */}
+										Ja
+									</Button>
+									<Button
+										className="neos-button-warning"
+										onClick={() => {
+											open.setFalse();
+											this.handleCancellation();
+										}}
+									>
+										{/* @TODO: I18n */}
+										Nein
+									</Button>
+								</ButtonList>
+							</Modal>
+						</Condition>
+						{this.props.children({
+							isOpen: open.is,
+							show: open.setTrue,
+							hide: open.setFalse
+						})}
+					</React.Fragment>
+				)}
+			</Toggle>
+		);
+	}
+}
