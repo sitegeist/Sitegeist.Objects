409d45c80aa6cd4d13d220285c058feaecbd37e3
Wilhelm Behncke <wilhelm.behncke@protonmail.com>
2018-05-21 08:01:42 +0200

    FEATURE: Create marker interface for Store

---
+++ Classes/Store.php
@@ -0,0 +1,18 @@
+<?php
+namespace Sitegeist\Objects;
+
+/*
+ * Copyright notice
+ *
+ * (c) 2018 Wilhelm Behncke <behncke@sitegeist.de>
+ * All rights reserved
+ *
+ * This file is part of the Sitegeist/Objects project under GPL-3.0.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.md file that was distributed with this source code.
+ */
+
+interface Store
+{
+}
