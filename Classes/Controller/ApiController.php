65f8f73a558ce1d5cd8a0847252d57001bffca92
Wilhelm Behncke <wilhelm.behncke@protonmail.com>
2018-05-21 07:31:50 +0200

    FEATURE: Create Endpoint for File Uploads

--- Classes/Controller/ApiController.php
+++ Classes/Controller/ApiController.php
@@ -15,7 +15,51 @@ namespace Sitegeist\Objects\Controller;
 
 use Neos\Flow\Annotations as Flow;
 use Wwwision\GraphQL\Controller\StandardController as GraphQlController;
+use Neos\Media\Domain\Repository\AssetRepository;
+use Neos\Media\Domain\Model\Asset;
 
 class ApiController extends GraphQlController
 {
+    /**
+     * @Flow\Inject
+     * @var AssetRepository
+     */
+    protected $assetRepository;
+
+    /**
+     * Initialization for uploadAction
+     *
+     * @return void
+     */
+    protected function initializeUploadAction()
+    {
+        $assetMappingConfiguration = $this->arguments->getArgument('asset')->getPropertyMappingConfiguration();
+        $assetMappingConfiguration->allowProperties('title', 'resource', 'assetCollections');
+        $assetMappingConfiguration->setTypeConverterOption(PersistentObjectConverter::class, PersistentObjectConverter::CONFIGURATION_CREATION_ALLOWED, true);
+        $assetMappingConfiguration->setTypeConverterOption(AssetInterfaceConverter::class, AssetInterfaceConverter::CONFIGURATION_ONE_PER_RESOURCE, true);
+
+        $assetMappingConfiguration->forProperty('assetCollections')
+            ->allowAllProperties();
+    }
+
+    /**
+     * Upload a new asset. No redirection and no response body, for use by plupload (or similar).
+     *
+     * @param Asset $asset
+     * @return string
+     */
+    public function uploadAction(Asset $asset)
+    {
+        if ($this->persistenceManager->isNewObject($asset)) {
+            $this->assetRepository->add($asset);
+            $this->response->setStatus(201);
+        } else {
+            $this->assetRepository->update($asset);
+            $this->response->setStatus(200);
+        }
+
+        return json_encode([
+            '__identity' => $this->persistenceManager->getIdentifierByObject($asset)
+        ]);
+    }
 }
