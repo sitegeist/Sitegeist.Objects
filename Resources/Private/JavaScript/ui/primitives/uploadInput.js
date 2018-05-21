b079b6039f8ab044e18dedd9ec6bc3657cb6f6b0
Wilhelm Behncke <wilhelm.behncke@protonmail.com>
2018-05-21 08:46:47 +0200

    FEATURE: Create UploadInput component

---
+++ Resources/Private/JavaScript/ui/primitives/uploadInput.js
@@ -0,0 +1,46 @@
+/*
+ * Copyright notice
+ *
+ * (c) 2018 Wilhelm Behncke <behncke@sitegeist.de>
+ * All rights reserved
+ *
+ * This file is part of the Sitegeist/Objects project under GPL-3.0.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.md file that was distributed with this source code.
+ */
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import Dropzone from 'react-dropzone';
+
+const UploadArea = styled.div`
+	padding: 20px!important;
+	border: 2px dashed #ccc;
+`;
+
+const UploadInput = ({allowMultiple, id, onChange}) => (
+	<Dropzone
+		style={{/*  */}}
+		multiple={allowMultiple}
+		onDrop={onChange}
+		inputProps={{id}}
+	>
+		<UploadArea disablePreview>
+			{/* @TODO: I18n */}
+			Drop files here
+		</UploadArea>
+	</Dropzone>
+);
+
+UploadInput.propTypes = {
+	allowMultiple: PropTypes.bool,
+	id: PropTypes.string.isRequired,
+	onChange: PropTypes.func.isRequired
+};
+
+UploadInput.defaultProps = {
+	allowMultiple: false
+};
+
+export default UploadInput;
