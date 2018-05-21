591bedc85d5cce6b479ef485e755c846dc71f6a8
Wilhelm Behncke <wilhelm.behncke@protonmail.com>
2018-05-21 08:16:51 +0200

    FEATURE: Add references and reference fields to RootQuery

--- Classes/GraphQl/RootQuery.php
+++ Classes/GraphQl/RootQuery.php
@@ -20,10 +20,13 @@ use Neos\Neos\Domain\Service\UserService;
 use GraphQL\Type\Definition\ObjectType;
 use GraphQL\Type\Definition\Type;
 use Wwwision\GraphQL\TypeResolver;
+use Flowpack\ElasticSearch\ContentRepositoryAdaptor\Eel\ElasticSearchQueryBuilder;
 use Sitegeist\Objects\GraphQl\Input\ContentContextInput;
 use Sitegeist\Objects\GraphQl\Query\StoreHelper;
 use Sitegeist\Objects\GraphQl\Query\StoreQuery;
 use Sitegeist\Objects\GraphQl\Query\NodeTypeQuery;
+use Sitegeist\Objects\GraphQl\Query\ReferenceHelper;
+use Sitegeist\Objects\GraphQl\Query\ReferenceQuery;
 
 class RootQuery extends ObjectType
 {
@@ -44,6 +47,12 @@ class RootQuery extends ObjectType
      * @var UserService
      */
     protected $userService;
+
+    /**
+     * @Flow\Inject
+     * @var ElasticSearchQueryBuilder
+     */
+    protected $queryBuilder;
 
     /**
      * @Flow\InjectConfiguration(path="rootNodeName")
@@ -127,6 +136,77 @@ class RootQuery extends ObjectType
                     ],
                     'resolve' => function($_, $arguments) {
                         return $this->nodeTypeManager->getNodeType($arguments['name']);
+                    }
+                ],
+                'references' => [
+                    'type' => Type::listOf($typeResolver->get(ReferenceQuery::class)),
+                    'description' => 'Find node references',
+                    'args' => [
+                        'context' => [
+                            'type' => Type::nonNull($typeResolver->get(ContentContextInput::class)),
+                            'description' => 'The content context for this query'
+                        ],
+                        'search' => [
+                            'type' => Type::nonNull(Type::string()),
+                            'description' => 'A search term to filter by'
+                        ],
+                        'searchRoot' => [
+                            'type' => Type::id()
+                        ],
+                        'nodeType' => [
+                            'type' => Type::string()
+                        ],
+                        'directChildrenOnly' => [
+                            'type' => Type::boolean()
+                        ]
+                    ],
+                    'resolve' => function($_, $arguments) {
+                        $contentContext = $this->contentContextFactory->create($arguments['context']);
+                        $searchRoot = array_key_exists('searchRoot', $arguments) && $arguments['searchRoot'] ?
+                            $contentContext->getNodeByIdentifier($arguments['searchRoot']) :
+                            $contentContext->getRootNode();
+
+                        $query = $this->queryBuilder->query($searchRoot);
+
+                        if (array_key_exists('nodeType', $arguments) && $arguments['nodeType']) {
+                            $query = $query->nodeType($arguments['nodeType']);
+                        }
+
+                        if (array_key_exists('directChildrenOnly', $arguments) && $arguments['directChildrenOnly']) {
+                            if (!array_key_exists('searchRoot', $arguments)) {
+                                throw new \InvalidArgumentException(
+                                    '"searchRoot" must be configured when using "directChildrenOnly" flag!',
+                                    1526825101
+                                );
+                            }
+
+                            $query = $query->exactMatch('__parentNode', $this->node->getIdentifier());
+                        }
+
+                        $query = $query->fullText($arguments['search'] . '*');
+
+                        foreach($query->execute() as $result) {
+                            yield new ReferenceHelper($result);
+                        }
+                    }
+                ],
+                'reference' => [
+                    'type' => $typeResolver->get(ReferenceQuery::class),
+                    'description' => 'Find a single node reference',
+                    'args' => [
+                        'context' => [
+                            'type' => Type::nonNull($typeResolver->get(ContentContextInput::class)),
+                            'description' => 'The content context for this query'
+                        ],
+                        'identifier' => [
+                            'type' => Type::id()
+                        ]
+                    ],
+                    'resolve' => function($_, $arguments) {
+                        $contentContext = $this->contentContextFactory->create($arguments['context']);
+                        $referenceNode = $contentContext->getNodeByIdentifier($arguments['identifier']);
+
+                        return new ReferenceHelper($referenceNode);
                     }
                 ]
             ]
