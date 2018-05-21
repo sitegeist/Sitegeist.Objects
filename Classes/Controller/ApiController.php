134d74e0256983ec20973307b23dffec3b863544
Wilhelm Behncke <wilhelm.behncke@protonmail.com>
2018-05-21 07:33:16 +0200

    FEATURE: Create endpoint for thumbnail generation

--- Classes/Controller/ApiController.php
+++ Classes/Controller/ApiController.php
@@ -15,8 +15,12 @@ namespace Sitegeist\Objects\Controller;
 
 use Neos\Flow\Annotations as Flow;
 use Wwwision\GraphQL\Controller\StandardController as GraphQlController;
+use Neos\Flow\Property\TypeConverter\PersistentObjectConverter;
+use Neos\Media\TypeConverter\AssetInterfaceConverter;
 use Neos\Media\Domain\Repository\AssetRepository;
 use Neos\Media\Domain\Model\Asset;
+use Neos\Media\Domain\Model\ThumbnailConfiguration;
+use Neos\Media\Domain\Service\AssetService;
 
 class ApiController extends GraphQlController
 {
@@ -25,6 +29,12 @@ class ApiController extends GraphQlController
      * @var AssetRepository
      */
     protected $assetRepository;
+
+    /**
+     * @Flow\Inject
+     * @var AssetService
+     */
+    protected $assetService;
 
     /**
      * Initialization for uploadAction
@@ -75,5 +85,29 @@ class ApiController extends GraphQlController
             'fileSize' => $asset->getResource()->getFileSize(),
             'mediaType' => $asset->getResource()->getMediaType()
         ]);
+    }
+
+    /**
+     * @param Asset $asset
+     * @param integer $width Desired width of the image
+     * @param integer $maximumWidth Desired maximum width of the image
+     * @param integer $height Desired height of the image
+     * @param integer $maximumHeight Desired maximum height of the image
+     * @param boolean $allowCropping Whether the image should be cropped if the given sizes would hurt the aspect ratio
+     * @param boolean $allowUpScaling Whether the resulting image size might exceed the size of the original image
+     * @param integer $quality Quality of the processed image
+     * @param boolean $async Whether the thumbnail can be generated asynchronously
+     */
+    public function thumbnailAction(Asset $asset, $width = null, $maximumWidth = null, $height = null, $maximumHeight = null, $allowCropping = false, $allowUpScaling = false, $async = false, $quality = null)
+    {
+        $thumbnailConfiguration = new ThumbnailConfiguration($width, $maximumWidth, $height, $maximumHeight, $allowCropping, $allowUpScaling, $async, $quality);
+        $thumbnailUriAndSize = $this->assetService->getThumbnailUriAndSizeForAsset($asset, $thumbnailConfiguration, $this->request);
+
+        if (!$thumbnailUriAndSize || !array_key_exists('src', $thumbnailUriAndSize)) {
+            $this->response->setStatus(404);
+            return;
+        }
+
+        $this->redirectToUri($thumbnailUriAndSize['src'], 0, 302);
     }
 }
