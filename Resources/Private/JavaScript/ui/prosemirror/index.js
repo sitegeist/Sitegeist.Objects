825e04061c222a3794c18ddcd6a3542478deb63d
Wilhelm Behncke <wilhelm.behncke@protonmail.com>
2018-05-25 08:09:15 +0200

    TASK: Refactor ProseMirror Integration

--- Resources/Private/JavaScript/ui/prosemirror/index.js
+++ Resources/Private/JavaScript/ui/prosemirror/index.js
@@ -11,23 +11,24 @@
  */
 import React from 'shim/react';
 import styled from 'shim/styled-components';
-import {HtmlEditor, MenuBar} from '@aeaton/react-prosemirror';
+import {HtmlEditor} from '@aeaton/react-prosemirror';
 
 import Toolbar from '../structures/toolbar';
 
 import {options, menu} from './config';
 
-const Container = styled.div`
+const AdjustProseMirrorStyles = styled.div`
 	.ProseMirror {
-		margin-top: .5em;
 		background: #3f3f3f;
-		padding: 1em;
 
 		h1 {
 			font-size: 5.06em;
 			line-height: 1.28em;
 			padding-top: 0.296em;
 			margin-bottom: 0.0247em;
+			&:last-child {
+				margin-bottom: 0;
+			}
 		}
 
 		h2 {
@@ -35,6 +36,9 @@ const Container = styled.div`
 			line-height: 1.44em;
 			padding-top: 0.389em;
 			margin-bottom: 0.0926em;
+			&:last-child {
+				margin-bottom: 0;
+			}
 		}
 
 		h3 {
@@ -42,6 +46,9 @@ const Container = styled.div`
 			line-height: 1.44em;
 			padding-top: 0.389em;
 			margin-bottom: 0.333em;
+			&:last-child {
+				margin-bottom: 0;
+			}
 		}
 
 		h4 {
@@ -49,6 +56,9 @@ const Container = styled.div`
 			line-height: 1.08em;
 			padding-top: 0.208em;
 			margin-bottom: 0.875em;
+			&:last-child {
+				margin-bottom: 0;
+			}
 		}
 
 		p {
@@ -56,6 +66,9 @@ const Container = styled.div`
 			line-height: 1.63em;
 			padding-top: 0.500em;
 			margin-bottom: 1.13em;
+			&:last-child {
+				margin-bottom: 0;
+			}
 		}
 
 		strong {
@@ -96,42 +109,59 @@ const Container = styled.div`
 	}
 `;
 
-const RichText = ({...props}) => (
+const Container = styled.div`
+	.ProseMirror {
+		margin-top: .5em;
+		padding: 1em;
+	}
+`;
+
+const processMenuRecursively = (menu, {state, dispatch}) => menu.map(item => {
+	switch (true) {
+		case 'items' in item: {
+			const {items, ...rest} = item;
+
+			return {
+				...rest,
+				items: processMenuRecursively(items, {state, dispatch})
+			};
+		}
+
+		default: {
+			const {action, isActive, ...rest} = item;
+
+			return {
+				...rest,
+				isActive: typeof isActive === 'function' ? isActive(state) : Boolean(isActive),
+				action: () => action(state, dispatch)
+			};
+		}
+	}
+});
+
+export const RichTextRenderer = ({children, ...props}) => (
 	<HtmlEditor
 		options={options}
-		render={({editor, state, dispatch}) => {
-			const processMenuRecursively = menu => menu.map(item => {
-				switch (true) {
-					case 'items' in item: {
-						const {items, ...rest} = item;
-
-						return {
-							...rest,
-							items: processMenuRecursively(items)
-						};
-					}
-
-					default: {
-						const {action, isActive, ...rest} = item;
-
-						return {
-							...rest,
-							isActive: typeof isActive === 'function' ? isActive(state) : Boolean(isActive),
-							action: () => action(state, dispatch)
-						};
-					}
-				}
-			});
-
-			return (
-				<Container>
-					<Toolbar groups={processMenuRecursively(menu)}/>
-					{editor}
-				</Container>
-			);
-		}}
+		render={({state, dispatch, editor, ...props}) => children({
+			...props,
+			menu: processMenuRecursively(menu, {state, dispatch}),
+			editor: (
+				<AdjustProseMirrorStyles>{editor}</AdjustProseMirrorStyles>
+			)
+		})}
 		{...props}
 	/>
+);
+
+const RichText = props => (
+	<RichTextRenderer {...props}>
+		{({editor, menu}) => (
+			<Container>
+				<Toolbar groups={menu}/>
+				{editor}
+			</Container>
+		)}
+	</RichTextRenderer>
 );
 
 export default RichText;
