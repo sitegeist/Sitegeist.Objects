797ae31c1d3254d47aaa32e9f378372f5c913725
Wilhelm Behncke <wilhelm.behncke@protonmail.com>
2018-06-06 19:32:12 +0200

    FEATURE: Create Bar primitive

---
+++ Resources/Private/JavaScript/lib/presentation/primitives/bar.js
@@ -0,0 +1,19 @@
+/*
+ * Copyright notice
+ *
+ * (c) 2018 Wilhelm Behncke <behncke@sitegeist.de>
+ * All rights reserved
+ *
+ * This file is part of the Sitegeist/Objects project under GPL-3.0.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.md file that was distributed with this source code.
+ */
+import styled from 'shim/styled-components';
+
+const Bar = styled.div`
+	display: flex;
+	justify-content: space-between;
+`;
+
+export default Bar;
